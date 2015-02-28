32863c8ded300a54bac7af757802738ba92277d8
DOTIN SCHOOL 3 <rsamira89@gmail.com>
2015-02-28 12:44:46 +0330

    update..

--- src/main/classes/ModifyServletClass.java
+++ src/main/classes/ModifyServletClass.java
@@ -21,6 +21,7 @@ public class ModifyServletClass extends HttpServlet {
 
 
     @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         if (request.getParameter("delete") != null) {
